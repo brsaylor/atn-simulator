d3aa1b94f386a31210a581af9b2076a36444e655
Ben Saylor <brsaylor@gmail.com>
2017-04-05 19:41:47 -0700

    Make ModelParameters initialize itself with defaults

--- src/main/java/edu/sfsu/worldofbalance/atnsimulator/ModelParameters.java
+++ src/main/java/edu/sfsu/worldofbalance/atnsimulator/ModelParameters.java
@@ -1,5 +1,15 @@
 package edu.sfsu.worldofbalance.atnsimulator;
 
+import java.util.Arrays;
+
+import static edu.sfsu.worldofbalance.atnsimulator.NodeAttributes.NodeType.PRODUCER;
+import static edu.sfsu.worldofbalance.atnsimulator.NodeAttributes.NodeType.CONSUMER;
+
+/**
+ * Data structure holding a set of parameters to be passed to ModelEquations.
+ *
+ * The link-level parameters are indexed as [predatorId][preyId].
+ */
 public class ModelParameters {
 
     // System-wide parameters
@@ -23,18 +33,38 @@ public class ModelParameters {
      * @param N number of nodes
      */
     public ModelParameters(int N) {
-        useSystemCarryingCapacity = false;
+        useSystemCarryingCapacity = Defaults.useSystemCarryingCapacity;
+        systemCarryingCapacity = Defaults.systemCarryingCapacity;
 
-        metabolicRate = new double[N];
-        growthRate = new double[N];
-        carryingCapacity = new double[N];
+        metabolicRate = vector(N, Defaults.metabolicRate);
+        growthRate = vector(N, Defaults.growthRate);
+        carryingCapacity = vector(N, Defaults.carryingCapacity);
 
-        maximumIngestionRate = new double[N][N];
-        predatorInterference = new double[N][N];
-        functionalResponseControl = new double[N][N];
-        relativeHalfSaturationDensity = new double[N][N];
-        halfSaturationDensity = new double[N][N];
-        assimilationEfficiency = new double[N][N];
+        maximumIngestionRate = matrix(N, Defaults.maximumIngestionRate);
+        predatorInterference = matrix(N, Defaults.predatorInterference);
+        functionalResponseControl = matrix(N, Defaults.functionalResponseControl);
+        relativeHalfSaturationDensity = matrix(N, Defaults.relativeHalfSaturationDensity);
+        halfSaturationDensity = matrix(N, Defaults.halfSaturationDensity);
+        assimilationEfficiency = matrix(N, Defaults.assimilationEfficiency);
+    }
+
+    public ModelParameters(FoodWeb foodWeb) {
+        this(foodWeb.nodeCount());
+
+        if (!foodWeb.nodeIdsAreNormalized())
+            throw new FoodWebNotNormalizedException();
+
+        // Assimilation efficiency default depends on whether prey is plant or animal
+        for (int nodeId : foodWeb.nodes()) {
+            switch (foodWeb.getNodeAttributes(nodeId).nodeType) {
+                case PRODUCER:
+                    fillColumn(assimilationEfficiency, nodeId, Defaults.assimilationEfficiencyPlant);
+                    break;
+                case CONSUMER:
+                    fillColumn(assimilationEfficiency, nodeId, Defaults.assimilationEfficiencyAnimal);
+                    break;
+            }
+        }
     }
 
     public static class Defaults {
@@ -51,8 +81,27 @@ public class ModelParameters {
         public static double relativeHalfSaturationDensity = 1;
         public static double halfSaturationDensity = 0.5;
 
-        // Default assimilation efficiency depends on whether prey node is a plant or animal
+        // Default assimilation efficiency depends on whether prey node is a plant or animal.
+        public static double assimilationEfficiency = 1;
         public static double assimilationEfficiencyPlant = 0.5;
         public static double assimilationEfficiencyAnimal = 0.8;
+    }
+
+    private double[] vector(int size, double value) {
+        double[] array = new double[size];
+        Arrays.fill(array, value);
+        return array;
+    }
+
+    private double[][] matrix(int size, double value) {
+        double[][] matrix = new double[size][size];
+        for (double[] row : matrix)
+            Arrays.fill(row, value);
+        return matrix;
+    }
+
+    private void fillColumn(double[][] matrix, int column, double value) {
+        for (int row = 0; row < matrix.length; row++)
+            matrix[row][column]  = value;
     }
 }
---
+++ src/test/java/edu/sfsu/worldofbalance/atnsimulator/ModelParametersTest.java
@@ -0,0 +1,48 @@
+package edu.sfsu.worldofbalance.atnsimulator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static edu.sfsu.worldofbalance.atnsimulator.NodeAttributes.NodeType.PRODUCER;
+import static edu.sfsu.worldofbalance.atnsimulator.NodeAttributes.NodeType.CONSUMER;
+
+// ModelParameters is an essentially behavior-less data structure,
+// but still needs testing, because it instantiates itself in nontrivial ways.
+public class ModelParametersTest {
+
+    @Test
+    public void testNodeCountConstructor() {
+        ModelParameters parameters = new ModelParameters(2);
+
+        double v = ModelParameters.Defaults.metabolicRate;
+        assertArrayEquals(new double[] {v, v}, parameters.metabolicRate, 1e-20);
+
+        assertEquals(parameters.assimilationEfficiency.length, 2);
+        v = ModelParameters.Defaults.assimilationEfficiency;
+        for (double[] row : parameters.assimilationEfficiency)
+            assertArrayEquals(new double[] {v, v}, row, 1e-20);
+    }
+
+    @Test
+    public void testFoodWebConstructor() {
+        FoodWeb web = new FoodWeb();
+        web.addNode(0);
+        web.setNodeAttributes(0, new NodeAttributes(PRODUCER));
+        web.addNode(1);
+        web.setNodeAttributes(1, new NodeAttributes(CONSUMER));
+        web.addNode(2);
+        web.setNodeAttributes(2, new NodeAttributes(CONSUMER));
+
+        ModelParameters parameters = new ModelParameters(web);
+
+        double v = ModelParameters.Defaults.metabolicRate;
+        assertArrayEquals(new double[] {v, v, v}, parameters.metabolicRate, 1e-20);
+
+        assertEquals(parameters.assimilationEfficiency.length, 3);
+        double p = ModelParameters.Defaults.assimilationEfficiencyPlant;
+        double a = ModelParameters.Defaults.assimilationEfficiencyAnimal;
+        for (double[] row : parameters.assimilationEfficiency)
+            assertArrayEquals(new double[] {p, a, a}, row, 1e-20);
+    }
+}
